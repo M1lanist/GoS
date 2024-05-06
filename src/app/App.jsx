40efddbc7b3da8b671021e8e4aab51b14c0f76bf
Kemel Zhumabaev <kemel.zhumabaev@timelyskills.com>
2024-05-06 18:01:25 +0600

    93248 input

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,15 +2,14 @@ import { RouterProvider } from "react-router-dom"
 import routes from "./providers/router/routes"
 
 import './globals.css'
-import Headline1 from "../shared/ui/ui-header/ui-typography/ui-typography";
+import { Input } from "@/shared/components/ui/input";
 
 function App() {
   
   return (
     <div>
       <RouterProvider router={routes} />
-      <Headline1 variant="h1">Sasori</Headline1>
-      
+      <Input type="email" placeholder="Введите Email"  />   
     </div>
   )
 }
