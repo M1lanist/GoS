201d5d8b7edb7d3d515d6b7679d3aa93ab23ec0d
Aidar Alapaev <aalapaev@mail.ru>
2024-05-21 20:41:44 +0600

    Created Routing permissions

--- src/app/providers/router/routes.jsx
+++ src/app/providers/router/routes.jsx
@@ -8,37 +8,104 @@ import BoardPage from "@/pages/BoardPage";
 import SchedulePage from "@/pages/SchedulePage";
 import LecturesPage from "@/pages/LecturesPage";
 
+
+
+const role = 'coach';
+
+// ! Я не стал писать запрос на users так как state manager еще не готов, иначе мне бы пришлось писать запрос в этом файле 
+
+
+const adminRoutes = [
+  {
+            path: "/",
+            element: <MainPage />,
+            errorElement: <ErrorPage />,
+          },
+          {
+            path: "/login",
+            element: <Autorization />,
+          },
+          {
+            path: "/myprofile",
+            element: <ProfilePage />,
+          },
+          {
+            path: "/myclass",
+            element: <MyClassPage />,
+          },
+          {
+            path: "/board",
+            element: <BoardPage />,
+          },
+          {
+            path: "/schedule",
+            element: <SchedulePage />,
+          },
+          {
+            path: "/lectures",
+            element: <LecturesPage />,
+          },
+]
+const coachRoutes = [
+  {
+            path: "/",
+            element: <MainPage />,
+            errorElement: <ErrorPage />,
+          },
+          {
+            path: "/login",
+            element: <Autorization />,
+          },
+          {
+            path: "/myprofile",
+            element: <ProfilePage />,
+          },
+          {
+            path: "/myclass",
+            element: <MyClassPage />,
+          },
+          // {
+          //   path: "/board",
+          //   element: <BoardPage />,
+          // },
+          // {
+          //   path: "/schedule",
+          //   element: <SchedulePage />,
+          // },
+          // {
+          //   path: "/lectures",
+          //   element: <LecturesPage />,
+          // },  
+          // ? В таске не указано к каким маршрутам есть доступ у студента и тренера, поэтому я оставил маршруты на свое усмотрение
+]
+const studRoutes = [
+  {
+            path: "/",
+            element: <MainPage />,
+            errorElement: <ErrorPage />,
+          },
+          {
+            path: "/login",
+            element: <Autorization />,
+          },
+          {
+            path: "/myprofile",
+            element: <ProfilePage />,
+          },
+      
+  
+]
+
+
 const routes = createBrowserRouter([
   {
     path: '/',
-    element: <MainPage />,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: '/login',
-    element: <Autorization />
-  },
-  {
-    path: '/myprofile',
-    element: <ProfilePage />,
-   
-  },
-  {
-    path: '/myclass',
-    element: <MyClassPage />
-  },
-  {
-    path: '/board',
-    element: <BoardPage />
-  },
-  {
-    path: '/schedule',
-    element: <SchedulePage />
-  },
-  {
-    path: '/lectures',
-    element: <LecturesPage />
+    children: role === 'admin' ? adminRoutes : (role === 'student' ? studRoutes : coachRoutes),
   },
 ]);
+
+
+
+
 
 export default routes;
